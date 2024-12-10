86533a6f6f112b0467507898758852237314a499
Tadiyos Mesfin <taddmesfin@gmail.com>
2024-12-10 16:33:43 +0300

    python number1 question

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print("Addition of " +str(number1)+" and "+str(number2)+" is "+str(addition))
+print("Subtraction of " +str(number1)+" and "+str(number2)+" is "+str(subtraction))
+print("Multiplication of " +str(number1)+" and "+str(number2)+" is "+str(multiplication))

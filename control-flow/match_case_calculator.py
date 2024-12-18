78e7195717bf730a774e3fcdab2a996b3b8e271f
Tadiyos Mesfin <taddmesfin@gmail.com>
2024-12-18 09:35:12 +0300

    new match  calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -13,8 +13,9 @@ match user_sign:
         result = num1 * num2
         print(f"The result is {result}.")
     case "/":
-        if num2 == 0:
-            print("Cannot divide by zero.")
-        else:
-            result = num1 / num2
-            print(f"The result is {result}.")
+        match num2:
+            case 0: 
+                print("Cannot divide by zero.")
+            case _:  
+                result = num1 / num2
+                print(f"The result is {result}.")

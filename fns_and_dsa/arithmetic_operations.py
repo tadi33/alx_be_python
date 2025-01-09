db0c0c1513ce890804447e5544eafc1be89ea9d8
Tadiyos Mesfin <taddmesfin@gmail.com>
2025-01-09 16:26:52 +0300

    arithmetic operation

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+def perform_operation(num1, num2, operation=['add', 'subtract', 'multiply','divide']):
+    if operation == 'add':
+        return num1 + num2 
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "num2 can not be zero to divide num1"
+        else:
+            return num1 / num2
+    else:
+        return "the operation you give is not available"
+    
+

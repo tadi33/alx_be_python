2094716e660450fd5ddab7c64014607da7242fae
Tadiyos Mesfin <taddmesfin@gmail.com>
2025-01-16 11:36:48 +0300

    update robust division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,7 +5,7 @@ def safe_divide(numerator, denominator):
         result = num / denom
         return result
     except ZeroDivisionError:
-        return "Error: Denominator cannot be zero."
+        return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Both numerator and denominator must be numeric values."
+        return "Error: Please enter numeric values only."
     
